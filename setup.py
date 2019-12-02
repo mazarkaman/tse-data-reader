f4a532fa80aa5bb9a5a89600c3e5f8544c1618ef
mazarkaman <azarkaman.net@gmail.com>
2019-12-02 12:44:54 +0330

    setup.py

--- setup.py
+++ setup.py
@@ -1,5 +1,5 @@
 import pathlib
-from setuptools import find_packages,setup
+from setuptools import find_packages, setup
 
 PATH = pathlib.Path(__file__).parent
 
@@ -8,29 +8,26 @@ README = (PATH / "README.md").read_text()
 setup(
     name="tse-data-reader",
     version="0.1.0",
-    description="Read the latest Real Python tutorials",
+    description="reads data from tsetmc.com website",
     long_description=README,
     long_description_content_type="text/markdown",
-    url="https://github.com/realpython/reader",
-    author="Real Python",
-    author_email="office@realpython.com",
+    url="https://github.com/mazarkaman/tse-data-reader.git",
+    author="M.Azarkaman",
+    author_email="Azarkaman.net@gmail.com",
     license="MIT",
     classifiers=[
-        "License :: OSI Approved :: MIT License",
         "Programming Language :: Python :: 3",
-        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.6",
     ],
     packages=find_packages(exclude=("tests",)),
     include_package_data=True,
     install_requires=[
         'pandas>=0.25.3,<0.26.0',
-        'requests>=2.22.0,<2.23.0'
-        'jdatetime>=3.6.2,<3.7.0'
+        'requests>=2.22.0,<2.23.0',
+        'jdatetime>=3.6.2,<3.7.0',
         'beautifulsoup4>=4.8.1,<4.9.0'
     ],
     project_urls={
-        "Bug Tracker": "https://bugs.example.com/HelloWorld/",
-        "Documentation": "https://docs.example.com/HelloWorld/",
-        "Source Code": "https://code.example.com/HelloWorld/",
+        "Source Code": "https://github.com/mazarkaman/tse-data-reader.git",
     }
 )
